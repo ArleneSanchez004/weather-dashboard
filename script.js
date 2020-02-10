0b64a3053ca524914a7e13e3382e985f725e41ea
Arlene Sanchez <arlene.sanchez004@gmail.com>
2020-02-10 11:45:22 -0500

    list appends city inputs, successful api calls

--- script.js
+++ script.js
@@ -4,16 +4,27 @@ var searchList = document.querySelector("#searchList");
 
 //create element to store city list vars
 var listOfCities = [];
-
+$( document ).ready(function() {
 $(submitBtn).click(function(event){
     event.preventDefault();
     //create li 
-    var newli = $("<li>").addClass("list-group-item");
+    //var newli = $("<li>").addClass("list-group-item");
+    var newlistChild = $("<li></li>").addClass("list-group-item");
     console.log($(city).val());
-    console.log(newli);
-    newli.text = $(city).val();
-    searchList.prepend(newli.text);
+    newlistChild.text = $(city).val();
+    searchList.append(newlistChild.text);
     searchList.innerHTML += ("<br>");
-   // newli.push(listOfCities);
+    infoGenerator();
+});
+
+function infoGenerator(){
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/weather?q=" + $(city).val() + "&appid=a4f825dd15cc236a136cd2fd592c0b68",
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+        
+      });
+};
 
 });
